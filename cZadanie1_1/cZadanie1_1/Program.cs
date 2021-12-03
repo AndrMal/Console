f4a4cc96c8ae47f48134de065dc2d649905edb9a
AndrMal <95502931+AndrMal@users.noreply.github.com>
2021-12-04 00:15:16 +0300

    Add files via upload

---
+++ cZadanie1_1/cZadanie1_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace cZadanie1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите V: ");
+                double V = double.Parse(Console.ReadLine());
+                Console.WriteLine("а = {0}", Math.Pow(V, 1 / 3.0));
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Нужно ввести число!");
+            }
+        }
+    }
+}
---
+++ cZadanie1_2/cZadanie1_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace cZadanie1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите число: ");
+                int a = int.Parse(Console.ReadLine());
+                if (a.ToString().Length != 2)
+                {
+                    throw new Exception();
+                }
+                if (((a/10) + (a%10)) % 2 == 0)
+                {
+                    Console.WriteLine("Сумма четная");
+                }
+                else
+                {
+                    Console.WriteLine("Сумма нечетная");
+                }
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Вы должны ввести число!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Вы должны ввести двузначное число!");
+            }
+        }
+    }
+}
