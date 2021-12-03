c078ad57dbd4e4a716ca5b79dd6bd43a43b1d2bc
AndrMal <95502931+AndrMal@users.noreply.github.com>
2021-12-03 23:03:59 +0300

    Add files via upload

---
+++ cZadanie3_1/cZadanie1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace cZadanie1
+{
+    class Program
+    {
+        static double f(double x)
+        {
+            if (x<0)
+            {
+                return 0;
+            }
+            else
+            {
+                return x / 2;
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите число: ");
+                double x = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Результат: {0}", f(x));
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Введено не число!");
+            }            
+        }
+    }
+}
---
+++ cZadanie3_2_3/cZadanie3_2_3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace cZadanie3_2_3
+{
+    class Program
+    {
+        public static void f(double a, double b, double h)
+        {
+            for (double x = a; x <= b; x += h)
+            {
+                if (x < 1)
+                {
+                    Console.WriteLine("f({0}) = {1}", x, (x * x - 1) * (x * x - 1));
+                }
+                else if (x > 1)
+                {
+                    Console.WriteLine("f({0}) = {1}", x, 1 / (1 + x * x));
+                }
+                else
+                {
+                    Console.WriteLine("f(1) = 0");
+                }
+            }
+        }
+        public static void f(double x, out double y)
+        {
+            if (x < 1)
+            {
+                y = (x * x - 1) * (x * x - 1);
+            }
+            else if (x > 1)
+            {
+                y = 1 / (1 + x * x);
+            }
+            else
+            {
+                y = 0;
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите a: ");
+                double a = Double.Parse(Console.ReadLine());
+                Console.Write("Введите b: ");
+                double b = Double.Parse(Console.ReadLine());
+                Console.Write("Введите h: ");
+                double h = Double.Parse(Console.ReadLine());
+                f(a, b, h);
+
+                Console.WriteLine("Задание 3");
+                for (double x = a; x <= b; x += h)
+                {
+                    double y;
+                    f(x, out y);
+                    Console.Write("{0} ", y);
+                }
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Введено не число!");
+            }
+        }
+    }
+}
