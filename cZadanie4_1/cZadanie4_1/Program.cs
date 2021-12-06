a656513b27734aef29d9027ec340153f95c6f5a7
AndrMal <95502931+AndrMal@users.noreply.github.com>
2021-12-06 13:12:41 +0300

    Add files via upload

---
+++ cZadanie4_1/cZadanie4_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace cZadanie4_1
+{
+    class Program
+    {
+        static double f(double N)
+        {
+            return N / sqr(N, 1);
+        }
+        static double sqr(double N, double i)
+        {
+            if (i > N)
+            {
+                return 0;
+            }
+            else
+            {
+                return Math.Sqrt(i + sqr(N, i + 1));
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите N: ");
+                double N = Double.Parse(Console.ReadLine());
+                if (N < 0)
+                {
+                    throw new Exception("N должно быть больше нуля!");
+                }
+                Console.WriteLine("Результат: {0}", f(N));
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Вы ввели что-то не то!");
+            }
+            catch(Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+        }
+    }
+}
---
+++ cZadanie4_2/cZadanie4_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace cZadanie4_2
+{
+    class Program
+    {
+        static string f(int N)
+        {
+            if (N==0)
+            {
+                return "0";
+            }
+            else if (N==1)
+            {
+                return "1";
+            }
+            else
+            {
+                if (N % 2 == 0)
+                {
+                    return f(N / 2) + "0";
+                }
+                else
+                {
+                    return f(N / 2) + "1";
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите целое число: ");
+                int N = int.Parse(Console.ReadLine());                
+                Console.WriteLine("Ответ: {0}",f(N));
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Вы ввели что-то не то!");
+            }
+        }
+    }
+}
